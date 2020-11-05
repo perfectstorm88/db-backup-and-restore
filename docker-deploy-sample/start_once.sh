2558e778120db4d64ad878ede4c0a1a0729a1f1b
perfectstorm88 <perfectstorm88@163.com>
2020-11-05 16:07:30 +0800

    add readme for docker

--- docker-deploy-sample/start_once.sh
+++ docker-deploy-sample/start_once.sh
@@ -1,1 +1,1 @@
- docker run -it -v $(pwd)/config:/usr/local/app/config -v $(pwd)/archive:/usr/local/app/archive  db-backup-and-restore python backup.py -t mysql1
+ docker run -it -v $(pwd)/config:/usr/local/app/config -v $(pwd)/archive:/usr/local/app/archive  db-backup-and-restore python backup.py -t 0
